bbc3197c7a5584fc0dfd1681ea5fa979ff415b16
Alexander Polyakov <polyakov.alx@gmail.com>
2019-09-29 23:33:26 +0300

    Change solve(...) API method's signature
    Now it also takes a boolean parameter 'silent' (default is False).
    If it is True, then the result of DPLL algorithm will be printed to
    STDOUT.
    
    Also, solve(...) method now always returns DPLL's output:
    (SAT, Model) / UNSAT

--- src/sat_solver.py
+++ src/sat_solver.py
@@ -1,3 +1,5 @@
+from typing import Tuple, Dict
+
 import argparse
 
 from cnf_formula import CNFFormula
@@ -18,20 +20,23 @@ def _create_arg_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def solve(expr: str) -> None:
+def solve(expr: str, silent: bool = False) -> Tuple[bool, Dict]:
     clauses, original_literals = tseitin(expr)
     cnf = CNFFormula(clauses)
     is_sat, model = dpll(cnf)
 
-    if is_sat:
-        model_str = '\n'.join(list(
-            map(lambda key: f'{key}: {model[key]}',
-                filter(lambda key: key in original_literals, model.keys()))
-        ))
-        print(f'SAT: {expr}\n'
-              f'Model: \n{model_str}')
-    else:
-        print('UNSAT')
+    if not silent:
+        if is_sat:
+            model_str = '\n'.join(list(
+                map(lambda key: f'{key}: {model[key]}',
+                    filter(lambda key: key in original_literals, model.keys()))
+            ))
+            print(f'SAT: {expr}\n'
+                  f'Model: \n{model_str}')
+        else:
+            print('UNSAT')
+
+    return is_sat, model
 
 
 if __name__ == '__main__':
